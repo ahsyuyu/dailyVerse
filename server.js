68e928615e02b17b8585aa048f2c0d46b211868d
ahsyuyu <ahsyuyu@gmail.com>
2016-07-11 19:43:37 -0700

    add package

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
+
 var VERSE_FILE = path.join(__dirname, 'verse.json');
 
 app.set('port', (process.env.PORT || 3000));
